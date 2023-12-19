88d6210b0e692cc524eefe491bc945d6f7bd431e
haraphat01 <haraphat@gmail.com>
2023-12-19 17:33:53 +0100

    updated the form's page'

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,3 +2,18 @@ import NextAuth from "next-auth"
 import AppleProvider from "next-auth/providers/apple"
 import GoogleProvider from "next-auth/providers/google"
 import EmailProvider from "next-auth/providers/email"
+
+export default NextAuth({
+    secret: process.env.SECRET,
+    providers: [
+      // OAuth authentication providers
+      AppleProvider({
+        clientId: process.env.APPLE_ID,
+        clientSecret: process.env.APPLE_SECRET,
+      }),
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+      }),
+    ],
+  })
