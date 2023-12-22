ef469ee1e9950b4b8dd742c6b4271cdc761d8013
haraphat01 <haraphat@gmail.com>
2023-12-22 18:11:37 +0100

    stored the users into the database

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from '../components/navbar';
-import { NextAuthProvider } from '../pages/Provider';
+
 
 const Layout = ({ children }) => {
   return (
--- pages/Provider.js
+++
@@ -1,5 +0,0 @@
-// "use client"
-// import { SessionProvider } from "next-auth/react"
-// export const NextAuthProvider = ({children,pageProps}) => {
-//     return <SessionProvider session={pageProps.session}>{children}</SessionProvider>
-// }
